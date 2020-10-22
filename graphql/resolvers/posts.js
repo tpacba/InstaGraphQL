e24ab9061990dace6df10ed9e57b4e093bf09d26
Tristan Pacba <tpacba@live.com>
2020-10-22 01:22:11 -0700

    added likePost resolver

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../utils/check-auth');
@@ -54,6 +54,26 @@ const posts = {
                 }
             } catch (error) {
                 throw new Error(err);
+            }
+        },
+        async likePost(_, { postId }, context) {
+            const user = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if(post) {
+                if(post.likes.find(like => like.username == user.username)) {
+                    post.likes = post.likes.filter(like => like.username != user.username);
+                } else {
+                    post.likes.push({
+                        username: user.username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+
+                await post.save();
+                return post;
+            } else {
+                throw new UserInputError("Post not found");
             }
         }
     }
