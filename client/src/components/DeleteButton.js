ee5614a03a91812266f23bbc0b8f9162a610e447
Tristan Pacba <tpacba@live.com>
2020-11-04 04:21:03 -0800

    update proxy for DeleteButton

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { gql, useMutation } from '@apollo/client';
 import { Icon, Button, Confirm } from 'semantic-ui-react';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 const DELETE_POST_MUTATION = gql`
     mutation deletePost($postId: ID!) {
@@ -12,8 +13,27 @@ const DeleteButton = ({ postId }) => {
     const [ confirmOpen, setConfirmOpen ] = useState(false);
 
     const [deletePost] = useMutation(DELETE_POST_MUTATION, {
-        update() {
+        update(proxy) {
             setConfirmOpen(false);
+
+            const data = proxy.readQuery({
+                query: FETCH_POSTS_QUERY
+            });
+
+            const newData = {getPosts: []}
+            newData.getPosts = data.getPosts.filter(post => post.id !== postId);
+
+            proxy.writeQuery({
+                query: FETCH_POSTS_QUERY,
+                newData
+            })
+
+            // data.getPosts = data.getPosts.filter(post => post.id !== postId);
+            // proxy.writeQuery({
+            //     query: FETCH_POSTS_QUERY,
+            //     data
+            // })
+
             window.location = "/";
         },
         variables: {
