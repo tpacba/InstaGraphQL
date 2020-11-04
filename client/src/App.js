f86260e8928e51b4bce09d1fe6c93a2f6e14c5b1
Tristan Pacba <tpacba@live.com>
2020-11-04 15:21:52 -0800

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import MenuBar from './components/MenuBar';
-import { Container } from 'semantic-ui-react';
+import { Container, Segment, Divider, Image, List } from 'semantic-ui-react';
 import { AuthProvider } from './context/auth';
 import AuthRoute from './util/AuthRoute';
 import SinglePost from './pages/SinglePost';
@@ -22,6 +22,18 @@ function App() {
           <AuthRoute exact path="/login" component={Login}></AuthRoute>
           <AuthRoute exact path="/register" component={Register}></AuthRoute>
           <Route exact path="/posts/:postId" component={SinglePost}></Route>
+          <Segment textAlign="center" style={{ margin: '5em 0em 0em', padding: '5em 0em' }} vertical>
+            <Divider section />
+            <Image centered size='small' src='./instagraphql_banner.png'/>
+            <List horizontal divided link size='small'>
+              <List.Item as='a' href='#'>
+                Github repository
+              </List.Item>
+              <List.Item as='a' href='#'>
+                About the developer
+              </List.Item>
+            </List>
+          </Segment>
         </Container>
       </Router>
     </AuthProvider>
